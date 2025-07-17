56b616222568802070cff4df54b897a9cd01e56a
Guru-24 <guru.g@s.amity.edu>
2025-07-17 21:57:08 +0530

    added the a code for student input details

--- prac3.c
+++ prac3.c
@@ -14,4 +14,17 @@ int main() {
 
     printf("Enter number of students: ");
     scanf("%d", &n);
+for (i = 0; i < n; i++) {
+        printf("\nEnter details for student %d:\n", i + 1);
+        printf("Roll Number: ");
+        scanf("%d", &students[i].rollNo);
+
+        printf("Name: ");
+        getchar();  // Clear newline from buffer
+        fgets(students[i].name, sizeof(students[i].name), stdin);
+        students[i].name[strcspn(students[i].name, "\n")] = '\0'; // Remove newline
+
+        printf("Marks: ");
+        scanf("%f", &students[i].marks);
+    }
 
